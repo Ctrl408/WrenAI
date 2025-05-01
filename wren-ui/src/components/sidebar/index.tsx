3f38974a55dae36860ef77d08bb29fd8b428f612
Christy Varughese mathew <101203618+Ctrl408@users.noreply.github.com>
2025-05-01 13:20:55 +0530

    Update index.tsx

--- wren-ui/src/components/sidebar/index.tsx
+++ wren-ui/src/components/sidebar/index.tsx
@@ -1,15 +1,11 @@
-import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { Button } from 'antd';
 import styled from 'styled-components';
 import { Path } from '@/utils/enum';
-import { DiscordIcon, GithubIcon } from '@/utils/icons';
 import SettingOutlined from '@ant-design/icons/SettingOutlined';
 import Home, { Props as HomeSidebarProps } from './Home';
 import Modeling, { Props as ModelingSidebarProps } from './Modeling';
 import Knowledge from './Knowledge';
-import APIManagement from './APIManagement';
-import LearningSection from '@/components/learning';
 
 const Layout = styled.div`
   position: relative;
@@ -64,10 +60,6 @@ const DynamicSidebar = (
       return <Knowledge />;
     }
 
-    if (pathname.startsWith(Path.APIManagement)) {
-      return <APIManagement />;
-    }
-
     return null;
   };
 
@@ -86,35 +78,10 @@ export default function Sidebar(props: Props) {
   return (
     <Layout className="d-flex flex-column">
       <DynamicSidebar {...props} pathname={router.pathname} />
-      <LearningSection />
       <div className="border-t border-gray-4 pt-2">
         <StyledButton type="text" block onClick={onSettingsClick}>
           <SettingOutlined className="text-md" />
           Settings
-        </StyledButton>
-        <StyledButton type="text" block>
-          <Link
-            className="d-flex align-center"
-            href="https://discord.com/invite/5DvshJqG8Z"
-            target="_blank"
-            rel="noopener noreferrer"
-            data-ph-capture="true"
-            data-ph-capture-attribute-name="cta_go_to_discord"
-          >
-            <DiscordIcon className="mr-2" style={{ width: 16 }} /> Discord
-          </Link>
-        </StyledButton>
-        <StyledButton type="text" block>
-          <Link
-            className="d-flex align-center"
-            href="https://github.com/Canner/WrenAI"
-            target="_blank"
-            rel="noopener noreferrer"
-            data-ph-capture="true"
-            data-ph-capture-attribute-name="cta_go_to_github"
-          >
-            <GithubIcon className="mr-2" style={{ width: 16 }} /> GitHub
-          </Link>
         </StyledButton>
       </div>
     </Layout>
